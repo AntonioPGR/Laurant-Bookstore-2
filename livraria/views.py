e794cf5fd2dfe2a885428571fd7cfd4a715e4987
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2023-03-25 23:55:18 -0300

    FEAT: main page

--- livraria/views.py
+++ livraria/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from os.path import join
+from livraria.models import Autor, Livro
 
 BASE_PATH = 'livraria/'
 
 def inicio(request):
-  return render(request, join(BASE_PATH, 'inicio.html'))
+  
+  livros = Livro.objects.all()[:6]
+  autores = Autor.objects.all()[:4]
+  
+  return render(request, join(BASE_PATH, 'inicio.html'), {
+    "livros": livros,
+    "autores": autores
+  })
